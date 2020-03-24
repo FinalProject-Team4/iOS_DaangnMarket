52eede29c63487a717debbd19a8a4d50876dd9ae
cskim <kcsol1005@gmail.com>
2020-03-24 22:33:07 +0900

    develop: RequestType >> Get DataRequest with associatedValue(Parameters)

---
+++ DaangnMarket/DaangnMarket/InitialStart/Controller/FindMyTownViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FindMyTownViewController.swift
+//  DaangnMarket
+//
+//  Created by cskim on 2020/03/24.
+//  Copyright © 2020 Jisng. All rights reserved.
+//
+
+import UIKit
+
+class FindMyTownViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
