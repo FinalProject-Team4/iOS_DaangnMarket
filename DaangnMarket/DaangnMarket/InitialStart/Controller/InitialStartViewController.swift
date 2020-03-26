29a8ffe9df6ebf909a6b3cee71176c5672d7b58e
cskim <kcsol1005@gmail.com>
2020-03-26 17:31:46 +0900

    modify: Initial Start Page >> elements moves to custom view

--- DaangnMarket/DaangnMarket/InitialStart/Controller/InitialStartViewController.swift
+++ DaangnMarket/DaangnMarket/InitialStart/Controller/InitialStartViewController.swift
@@ -11,22 +11,8 @@ import UIKit
 class InitialStartViewController: UIViewController {
   // MARK: Views
   
-  private let animationView = DaangnAnimationView()
-  private let titleLabel = UILabel().then {
-    $0.text = "우리 동네 중고 직거래 당근마켓"
-    $0.textAlignment = .center
-    $0.font = .systemFont(ofSize: 20, weight: .bold)
-  }
-  private let subTitleLabel = UILabel().then {
-    $0.text = "당근마켓은 동네 직거래 마켓이에요.\n 내 동네를 설정하고 시작해보세요!"
-    $0.textAlignment = .center
-    $0.numberOfLines = 2
-    $0.font = .systemFont(ofSize: 16)
-  }
-  private lazy var startButton = DGButton(title: "내 동네 설정하고 시작하기").then {
-    $0.addTarget(self, action: #selector(didTapStartButton(_:)), for: .touchUpInside)
-    $0.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
-    $0.layer.cornerRadius = 8
+  private lazy var initialView = InitialStartView().then {
+    $0.addTarget(self, action: #selector(didTapStartButton(_:)))
   }
   
   // MARK: Life Cycle
@@ -39,56 +25,23 @@ class InitialStartViewController: UIViewController {
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     self.navigationController?.isNavigationBarHidden = true
-    self.animationView.animate()
+    self.initialView.animate()
   }
   
   // MARK: Initialize
   
   private func setupUI() {
-    self.setupAttributes()
+    self.view.backgroundColor = .systemBackground
     self.setupConstraints()
   }
   
-  private func setupAttributes() {
-    self.view.backgroundColor = .systemBackground
-  }
-  
   private func setupConstraints() {
-    let paddingY: CGFloat = 120
-    let spacing: CGFloat = 16
-    let buttonSize: CGFloat = 48
-    
-    self.animationView
+    self.initialView
       .then { self.view.addSubview($0) }
       .snp
       .makeConstraints {
-        $0.centerX.equalToSuperview()
-        $0.top.equalToSuperview().offset(paddingY)
-        $0.size.equalTo(self.view.safeAreaLayoutGuide.snp.width)
-      }
-    self.titleLabel
-      .then { self.view.addSubview($0) }
-      .snp
-      .makeConstraints {
-        $0.top.equalTo(self.animationView.snp.bottom).offset(spacing)
-        $0.centerX.equalTo(self.animationView)
-      }
-    self.subTitleLabel
-      .then { self.view.addSubview($0) }
-      .snp
-      .makeConstraints {
-        $0.top.equalTo(self.titleLabel.snp.bottom).offset(spacing / 2)
-        $0.centerX.equalTo(self.titleLabel)
-    }
-    self.startButton
-      .then { self.view.addSubview($0) }
-      .snp
-      .makeConstraints {
-        $0.top.equalTo(self.subTitleLabel.snp.bottom).offset(spacing * 2)
-        $0.leading.trailing
-          .equalToSuperview()
-        .inset(UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing))
-        $0.height.equalTo(buttonSize)
+        $0.center.equalTo(self.view)
+        $0.width.equalTo(self.view.safeAreaLayoutGuide)
     }
   }
   
