e793cbd0fb3510021e11845c3ba4590f2d4ef600
cskim <kcsol1005@gmail.com>
2020-04-04 17:10:17 +0900

    develop: UIViewController for use easily UIAlertController

---
+++ DaangnMarket/DaangnMarket/Common/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UIAlertController+Extension.swift
+//  DaangnMarket
+//
+//  Created by cskim on 2020/04/03.
+//  Copyright © 2020 Jisng. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+  func presentAlert(
+    title: String = "",
+    message: String = "",
+    actions: [UIAlertAction] = [UIAlertAction(title: "확인", style: .default)]
+  ) {
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    actions.forEach { alert.addAction($0) }
+    self.present(alert, animated: true)
+  }
+}
